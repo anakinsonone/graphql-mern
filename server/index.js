a420365ada40ac2590293645cb3ec2445d0fe0c1
anakinsonone <akkhi.sonone@gmail.com>
2023-03-07 16:21:42 +0530

    done with backend

--- server/index.js
+++ server/index.js
@@ -12,11 +12,11 @@ const app = express();
 connectDB();
 
 app.use(
-    "/graphql",
-    graphqlHTTP({
-        schema,
-        graphiql: process.env.NODE_ENV === "development",
-    })
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
 );
 
 app.listen(port, console.log(`Server running on port ${port}`));
